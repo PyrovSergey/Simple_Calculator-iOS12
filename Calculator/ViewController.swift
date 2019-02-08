c3281188577fd581b2d09d8746a4e30a69f1748b
serge <pyrov.sergey@gmail.com>
2019-02-08 23:55:36 +0300

    Add work screen and basics functions

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -2,8 +2,6 @@
 //  ViewController.swift
 //  Calculator
 //
-//  Created by Angela Yu on 10/09/2018.
-//  Copyright © 2018 London App Brewery. All rights reserved.
 //
 
 import UIKit
@@ -12,20 +10,59 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
+    private var isFinishedTypingNymber: Bool = true
+    
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else { fatalError("Cannot convert display label text to a Double") }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+
+    }
     
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
-    
+        isFinishedTypingNymber = true
+        
+        if let calcMethod = sender.currentTitle {
+            switch calcMethod {
+            case "+/-" : displayValue *= -1
+            case "%" : displayValue /= 100
+            case "AC" : displayLabel.text = "0"
+            default: break
+            }
+        }
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
-        
-        //What should happen when a number is entered into the keypad
     
+        //What should happen when a number is entered into the keypad
+        if let numValue = sender.currentTitle {
+            if let _ = displayLabel.text {
+                if isFinishedTypingNymber && numValue != "." {
+                    displayLabel.text = numValue
+                    isFinishedTypingNymber = false
+                } else {
+                    if numValue == "." {
+                        
+                        let isInt = floor(displayValue) == displayValue
+                        
+                        if !isInt {
+                            return
+                        }
+                    }
+                    displayLabel.text = displayLabel.text! + numValue
+                }
+            }
+        }
     }
-
+    
+    
 }
 
